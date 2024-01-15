424e2be4ec27f71297fbc575cb597ec1f7860444
LohanAbde <152969679+LohanAbde@users.noreply.github.com>
2024-01-15 14:35:07 +0100

    Avec les collisions en plus

--- main.py
+++ main.py
@@ -2,41 +2,40 @@ import pygame
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((1080, 720))
 clock = pygame.time.Clock()
 running = True
 dt = 0
 
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+player_radius = 40
 
 while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    screen.fill("brown")
 
-    pygame.draw.circle(screen, "red", player_pos, 40)
+    pygame.draw.circle(screen, "red", player_pos, player_radius)
 
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
+    player_speed = 300  # On peut ajuster la vitesse de notre perso
+
+    if keys[pygame.K_z]:
+        player_pos.y -= player_speed * dt
     if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
-    if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
+        player_pos.y += player_speed * dt
+    if keys[pygame.K_q]:
+        player_pos.x -= player_speed * dt
     if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
+        player_pos.x += player_speed * dt
 
-    # flip() the display to put your work on screen
+    # Permet de bloquer le joueur à la limite de la fenêtre
+    player_pos.x = max(player_radius, min(player_pos.x, screen.get_width() - player_radius))
+    player_pos.y = max(player_radius, min(player_pos.y, screen.get_height() - player_radius))
+
     pygame.display.flip()
-
-    # limits FPS to 60
-    # dt is delta time in seconds since last frame, used for framerate-
-    # independent physics.
     dt = clock.tick(60) / 1000
 
 pygame.quit()
